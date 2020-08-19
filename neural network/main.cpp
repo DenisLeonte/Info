96ccbb2880eec2b05f03dc9e30f892bd5a6b82a2
MyBoyDoru <denis.leonte123@gmail.com>
2020-08-19 06:52:18 +0100

    Begun neural net tutorial at min 26:10

---
+++ neural network/main.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Neuron;
+
+typedef vector<Neuron> Layer;
+
+class Neuron
+{
+public:
+    Neuron();
+
+private:
+    double m_outputVal;
+    vector<double> m_outputWeights;
+}
+
+class Net
+{    
+public:
+    Net(const  vector<unsigned> &topology);
+    void feedForward(const vector<double> &inputVals){};
+    void backProp(const vector<double> &targetVals){};
+    void getResults(vector<double> &resultVals) const {};
+
+private:
+    vector<Layer> m_layers;
+};
+
+Net::Net(const  vector<unsigned> &topology)
+{
+    unsigned numLayers = topology.size();
+    for(unsigned layerNum = 0; layerNum < numLayers; ++layerNum)
+    {
+        m_layers.push_back(Layer());
+
+        for(unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum){
+                m_layers.back().push_back(Neuron());
+                cout<< "Made a neuron" <<endl;
+            }
+    }
+}
+
+int main(){
+
+    vector<unsigned> topology;
+    topology.push_back(3);
+    topology.push_back(2);
+    topology.push_back(1);
+    Net myNet(topology);
+
+    vector<double> inputVals;
+    myNet.feedForward(inputVals);
+
+    vector<double> targetVals;
+    myNet.backProp(targetVals);
+
+    vector<double> resultVals;
+    myNet.getResults(resultVals);
+
+    return 0;
+}
